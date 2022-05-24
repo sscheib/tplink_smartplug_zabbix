a49cb55396accc004e8b3f791dbf5ae6afb30a53
Steffen Scheib <steffen@scheib.me>
2022-05-24 18:23:21 +0200

    ~ Renamed function gather_values to tplSmartplugQuery::gather_values to be consistent throughout    the code ~ Redirect error output to stderr ~ Added support for TP-Link KP115 (EU) ~ Bumped version to 1.3

--- tplink_smartplug.sh
+++ tplink_smartplug.sh
@@ -4,7 +4,8 @@
 # Description:
 #   Queries a TP-Link Smartplug and sends the data to a Zabbix server.
 #   Currently supported models:
-#     - HS110
+#     - HS110 (EU)
+#     - KP115 (EU)
 #  
 # Exit codes:
 #   0: Successfully queried TP-Link Smartplug and send the data to Zabbix server
@@ -12,7 +13,7 @@
 #   2: Failed to parse commandline options
 #   3: TP-Link Smartplug hostname or IP address not given
 #   4: Zabbix server hostname or IP address not given
-#   5: One or more required binaries are missing
+#   5: Initialization of the script failed
 #   6: Failed to retrieve or send an item from Smartplug/to Zabbix server
 #
 # log file                                      : no
@@ -33,6 +34,14 @@
 #
 # Changelog:
 # 24.05.2022:
+#  ~ Renamed function gather_values to tplSmartplugQuery::gather_values to be consistent throughout 
+#    the code
+#  ~ Redirect error output to stderr
+#  ~ Added support for TP-Link KP115 (EU)
+#  ~ Bumped version to 1.3
+#  
+#
+# 24.05.2022:
 #  + Added the ability to use the environment variable VERBOSE to enable
 #    verbostity (additionally to --verbose)
 #  ~ Bumped version to 1.2
@@ -45,11 +54,11 @@
 #
 # 23.05.2022: . Initial
 #
-# version: 1.2
-VERSION=1.2
+# version: 1.3
+VERSION=1.3
 
 # option definitions for getopt
-__LONG_OPTIONS="hostname:,zabbix-server:,help,verbose"
+__LONG_OPTIONS="host:,zabbix-server:,help,verbose"
 __SHORT_OPTIONS="z:,n:,h,v"
 
 # verbose output, disabled by default
@@ -89,6 +98,27 @@ declare -ar __ITEMS=(
   "voltage_mv"
 )
 
+# holds additional (to those defined in __ITEMS) items per hardware model
+# format: ["<hw_model>"]="<item>[:<mapped_to>],<item>[:<mapped_to>],<item>"
+# Note:
+#   - <hw_model> is the model name as determined in tplSmartplugQuery::determine_hardware_model
+#   - the optional <:mapped_to> sets the retrieved value to the item specified after the colon 
+#     in the Zabbix template
+#     -> Example: "mic_type:model", will map the value of 'mic_type' to the Zabbix item 'model'
+declare -Ar __HARDWARE_MODEL_ADDITIONAL_ITEMS=(
+  ["KP115_EU_"]="mic_type:type,ntc_state,obd_src,status"
+) 
+
+# defines the number of lines to retrieve from 'kasa sysinfo'
+# format: ["<hw_model>"]="<num_lines>"
+# Note:
+#   - <hw_model> is the model name as determined in tplSmartplugQuery::determine_hardware_model
+#   - <num_lines> specifies the number of lines to gather from 'kasa sysinfo'
+declare -Ar __HARDWARE_MODEL_NUM_RETRIEVE_LINES=(
+  ["HS110_EU_"]=23
+  ["KP115_EU_"]=25
+)
+
 # let the whole pipe fail (exit with != 0) if a command in it fails
 set -o pipefail
 
@@ -102,6 +132,10 @@ declare -ar __REQUIRED_BINARIES=(
 declare __ENERGY_METER=""
 # used to store system information (kasa sysinfo) in order to minimize querying the smartplug
 declare __SYSTEM_INFO=""
+# used to store the hardware model
+declare __HARDWARE_MODEL=""
+# used to store the number of lines to retrieve when using 'kasa sysinfo'
+declare -i __NUM_RETRIEVE_LINES=-1
 
 ###
 # function tplSmartplugQuery::init
@@ -121,6 +155,11 @@ declare __SYSTEM_INFO=""
 #   #  | name                                          | access-type | notes
 #------+-----------------------------------------------+-------------+-------------------------------------------------< 
 #   01 | __REQUIRED_BINARIES                           | read        | --
+#   02 | __HOSTNAME                                    | read        | --
+#   03 | __HARDWARE_MODEL_NUM_RETRIEVE_LINES           | read        | --
+#   04 | __HARDWARE_MODEL                              | read        | --
+#   05 | __NUM_RETRIEVE_LINES                          | read/write  | --
+#   06 | __HARDWARE_MODEL_ADDITIONAL_ITEMS             | read        | --
 #---
 # Return values:
 #---
@@ -128,21 +167,61 @@ declare __SYSTEM_INFO=""
 #--------------+-------------------------------------------------------------------------------------------------------< 
 # (return)   0 | If all conditions to run this script are met
 # (return)   1 | If one or more binaries defined in __REQUIRED_BINARIES are missing
+# (return)   2 | If we were unable to determine the hardware model 
+# (return)   3 | If __HARDWARE_MODEL is not (properly) defined in __HARDWARE_MODEL_NUM_RETRIEVE_LINES
+# (return)   4 | If a malformed item in __HARDWARE_MODEL_ADDITIONAL_ITEMS is found
 #####
 function tplSmartplugQuery::init () {
   for binary in "${__REQUIRED_BINARIES[@]}"; do
     command -v "${binary}" &> /dev/null || {
-      echo "ERROR: Binary '${binary}' is not installed, but required by this script!"; 
+      echo "ERROR: Binary '${binary}' is not installed, but required by this script!" >&2; 
       return 1; 
     };
   done
+
+  declare -i returnCode=-1
+  # determine hardware model
+  tplSmartplugQuery::determine_hardware_model "${__HOSTNAME}"
+  returnCode="${?}"
+  [[ "${returnCode}" -eq 0 ]] || {
+    return 2;
+  };
+
+  # set the lines to retrieve according to the hardware model
+  for model in "${!__HARDWARE_MODEL_NUM_RETRIEVE_LINES[@]}"; do
+    [[ "${model}" =~ ${__HARDWARE_MODEL} ]] || {
+      continue;
+    };
+
+    # found hardware model
+    __NUM_RETRIEVE_LINES="${__HARDWARE_MODEL_NUM_RETRIEVE_LINES["${model}"]}"    
+  done
+
+  # __NUM_RETRIEVE_LINES should differ by now from the default of -1
+  [[ "${__NUM_RETRIEVE_LINES}" -ne -1 ]] || {
+    return 3;
+  };
+
+  declare -i malformedItemFound=1
+  for item in "${__HARDWARE_MODEL_ADDITIONAL_ITEMS[@]}"; do
+    [[ "${item}" =~ ^([[:alnum:]_]+):?([[:alnum:]_]+,?){1,}$ ]] || {
+      echo "ERROR: Item definition in __HARDWARE_MODEL_ADDITIONAL_ITEMS malformed!" >&2;
+      echo "ERROR: The following line seems to be malformed:" >&2;
+      echo " line: '${item}'" >&2;
+      malformedItemFound=0;
+    };
+  done
+
+  [[ "${malformedItemFound}" -ne 0 ]] || {
+    return 4;
+  };
 
   return 0;
 }; # function tplSmartplugQuery::init ( )
 
 
 ###
-# function tplSmartplugQuery::init
+# function tplSmartplugQuery::gather_values
 #---
 # Description:
 #---
@@ -158,6 +237,7 @@ function tplSmartplugQuery::init () {
 #<  $1> | item                                  | string      | Item to query from the smartplug
 #<  $2> | host                                  | string      | Hostname or IP address of the smartplug
 #<  $3> | zabbixServer                          | string      | Hostname or IP address of the Zabbix server
+#[  $4] | mapItem                               | string      | Name of the key the item should be mapped to
 #---
 # Global variables:
 #---
@@ -165,6 +245,8 @@ function tplSmartplugQuery::init () {
 #------+-----------------------------------------------+-------------+-------------------------------------------------< 
 #   01 | __ENERGY_METER                                | read/write  | Variable to store the queried data from emeter
 #   02 | __SYSTEM_INFO                                 | read/write  | Variable to store the queried data from sysinfo
+#   03 | __NUM_RETRIEVE_LINES                          | read        | --
+#   03 | __VERBOSE                                     | read        | --
 #---
 # Return values:
 #---
@@ -176,7 +258,7 @@ function tplSmartplugQuery::init () {
 # (return)   3 | Parameter $3 (zabbixServer) not given
 # (return)   4 | Unknown item to query retrieved
 #####
-function gather_values() {
+function tplSmartplugQuery::gather_values() {
   [[ -n "${1}" ]] || {
     return 1;
   };
@@ -191,6 +273,12 @@ function gather_values() {
     return 3;
   };
   declare zabbixServer="${3}"
+
+  # set the mapItem as default to the retrieving item and override if $4 is given
+  declare mapItem="${item}"
+  [[ -z "${4}" ]] || {
+    mapItem="${4}"
+  };
 
   declare value=""
   case "${item}" in
@@ -221,15 +309,15 @@ function gather_values() {
       # get the current values from the device (via the emeter function), replace the description (via extractKeyword) as well as any given unit symbol and round it to 2 decimal digits
       value="$(printf "%.2f\n" "$(echo "${__ENERGY_METER}" | grep -E "^${extractKeyword}" | sed "s/^${extractKeyword}//" | sed -E 's/[[:space:]](A|V|W|kWh)//')")"
     ;;
-    # the below items can be retrieved as JSON from the smartplug
-    active_mode|alias|dev_name|deviceId|err_code|feature|fwId|hwId|hw_ver|icon_hash|latitude_i|led_off|longitude_i|mac|model|next_action|oemId|on_time|relay_state|rssi|sw_ver|type|updating) 
-      # query the smartplug only if __ENERGY_METER is empty
+    # all other values should be retrievable via kasa sysinfo
+    *)
+      # query the smartplug only if __SYSTEM_INFO is empty
       [[ -n "${__SYSTEM_INFO}" ]] || {
         __SYSTEM_INFO="$(kasa --type plug --host "${host}" sysinfo)";
-      }
+      };
 
-      # get the last 23 lines from sysinfo, replace ' with " (to have proper json) and get the value for the provided key
-      value="$(echo "${__SYSTEM_INFO}" | tail -n 23 | tr "\'" "\"" | jq -r ".${item}")"
+      # get the last lines from sysinfo, replace ' with " (to have proper json) and get the value for the provided key
+      value="$(echo "${__SYSTEM_INFO}" | tail -n "${__NUM_RETRIEVE_LINES}" | tr "\'" "\"" | jq -r ".${item}")"
 
       # special treatment for certain items
       case "${item}" in
@@ -256,19 +344,15 @@ function gather_values() {
       # finally store the value
       value="$(printf "%s\n" "${value}")"
     ;;
-    *)
-      # unknown item given
-      return 4;
-    ;;
   esac
 
   if [[ "${__VERBOSE}" -ne 0 ]]; then
-    echo "- tplink_smartplug_hs110[${item}] ${value}" | zabbix_sender -i - -s "${host}" -z "${zabbixServer}" >> /dev/null || {
+    echo "- tplink_smartplug[${mapItem}] ${value}" | zabbix_sender -i - -s "${host}" -z "${zabbixServer}" >> /dev/null || {
       # sending values to Zabbix failed
       return 5;
     };
   else
-    echo "- tplink_smartplug_hs110[${item}] ${value}" | zabbix_sender -i - -s "${host}" -z "${zabbixServer}" -vv || {
+    echo "- tplink_smartplug[${mapItem}] ${value}" | zabbix_sender -i - -s "${host}" -z "${zabbixServer}" -vv || {
       # sending values to Zabbix failed
       return 6;
     };
@@ -276,7 +360,36 @@ function gather_values() {
 
   # everything went fine
   return 0;
-} #; gather_values ()
+} #; tplSmartplugQuery::gather_values ( <item>, <host>, <zabbix_server> )
+
+function tplSmartplugQuery::determine_hardware_model () {
+  [[ -n "${1}" ]] || {
+    return 1;
+  };
+  declare host="${1}"
+  # query the smartplug only if __SYSTEM_INFO is empty
+  [[ -n "${__SYSTEM_INFO}" ]] || {
+    __SYSTEM_INFO="$(kasa --type plug --host "${host}" sysinfo)";
+  }
+
+  # retrieve the hardware model
+  __HARDWARE_MODEL="$(echo "${__SYSTEM_INFO}" | grep -E "^[[:space:]]+'model':" | awk '{print $2}')"
+  
+  # sanitize the hardware model:
+  # - remove trailing comma
+  # - remove all occurrences of '
+  # - replace all characters but alphanumeric ones (0-9 + A-z) with an underscore
+  # Example:
+  # HS110(EU) -> HS110_EU_
+  # KP115(EU) -> KP115_EU_
+  __HARDWARE_MODEL="$(echo "${__HARDWARE_MODEL}" | sed 's/,//' | tr -d "'" | sed 's/[^[:alnum:]]/_/g')"
+  [[ -n "${__HARDWARE_MODEL}" ]] || {
+    return 2;
+  };
+  
+  # everything went fine
+  return 0;
+} #; tplSmartplugQuery::determine_hardware_model ( <host> )
 
 function tplSmartplugQuery::usage ( ) {
   echo "Usage of "$(basename "${0}")""
@@ -301,14 +414,14 @@ returnCode="${?}"
 
 # getopt returns 4 if everything is fine
 [[ "${returnCode}" -eq 4 ]] || {
-  echo "ERROR: getopt --test failed!";
+  echo "ERROR: getopt --test failed!" >&2;
   exit 1;
 };
 
 # parse the commandline options
 __PARSED_OPTIONS="$(getopt --options="${__SHORT_OPTIONS}" --longoptions="${__LONG_OPTIONS}" --name "$0" -- "$@")"
 [[ "${?}" -eq 0 ]] || {
-  echo "ERROR: Parsing commandline options failed!";
+  echo "ERROR: Parsing commandline options failed!" >&2;
   exit 2;
 };
 
@@ -325,7 +438,7 @@ while true; do
       # assuming nothing more to do once the help is requested
       exit 0;
     ;;
-    -n|--hostname)
+    -n|--host)
       __HOSTNAME="$2"
       shift 2
     ;;
@@ -349,14 +462,14 @@ while true; do
 done
 
 [[ -n "${__HOSTNAME}" ]] || {
-  echo "ERROR: TP-Link Smartplug hostname or IP not given, although required!";
-  echo "Please use --hostname <value> or -n <value> to pass the hostname or IP address of the TP-Link Smartplug to this script."
+  echo "ERROR: TP-Link Smartplug hostname or IP not given, although required!" >&2;
+  echo "Please use --host <value> or -n <value> to pass the hostname or IP address of the TP-Link Smartplug to this script." >&2;
   exit 3;
 };
 
 [[ -n "${__ZABBIX_SERVER}" ]] || {
-  echo "ERROR: Zabbix server hostname or IP not given, although required!";
-  echo "Please use --zabbix-server <value> or -z <value> to pass the hostname or IP address of the Zabbix server to this script."
+  echo "ERROR: Zabbix server hostname or IP not given, although required!" >&2;
+  echo "Please use --zabbix-server <value> or -z <value> to pass the hostname or IP address of the Zabbix server to this script." >&2;
   exit 4;
 };
 
@@ -371,9 +484,9 @@ returnCode="${?}"
 declare -i itemFailed=1
 declare -a itemFailedReturnCodes=()
 
-# iterate over all items and send the data to Zabbix
+# iterate over all default items and send the data to Zabbix
 for item in "${__ITEMS[@]}"; do
-  gather_values "${item}" "${__HOSTNAME}" "${__ZABBIX_SERVER}"
+  tplSmartplugQuery::gather_values "${item}" "${__HOSTNAME}" "${__ZABBIX_SERVER}"
   returnCode="${?}"
 
   [[ "${returnCode}" -eq 0 ]] || {
@@ -382,13 +495,50 @@ for item in "${__ITEMS[@]}"; do
   };
 done
 
+# iterate over all "special" items - if the model has any
+for model in "${!__HARDWARE_MODEL_ADDITIONAL_ITEMS[@]}"; do
+  [[ "${model}" =~ ${__HARDWARE_MODEL} ]] || {
+    continue;
+  };
+
+  declare itemList="${__HARDWARE_MODEL_ADDITIONAL_ITEMS["${model}"]}"
+  # read in the itemList as array with the field seperator ','
+  IFS="," read -ra items <<< "${itemList}"
+
+  # iterate over the items
+  for item in "${items[@]}"; do
+    # unset mapItem in case it is still set
+    [[ -n "${mapItem}" ]] && unset mapItem
+
+    # set the mapItem to be item by default and override it only if required
+    mapItem="${item}"
+
+    # extract the mapped value if present
+    [[ ! "${item}" =~ : ]] || {
+      mapItem="$(echo "${item}" | awk -F ':' '{print $2}')"
+      item="$(echo "${item}" | awk -F ':' '{print $1}')"
+    };
+
+    # finally gather and send the values to Zabbix    
+    tplSmartplugQuery::gather_values "${item}" "${__HOSTNAME}" "${__ZABBIX_SERVER}" "${mapItem}"
+    returnCode="${?}"
+    [[ "${returnCode}" -eq 0 ]] || {
+      itemFailed=0;
+      itemFailedReturnCodes+=("${returnCode}")
+    };
+  done
+done
+
 # at least one item failed, exit accordingly
 [[ "${itemFailed}" -ne 0 ]] || {
-  echo "ERROR: One or more items failed to either be retrieved from the smartplug or failed to send to the Zabbix server."
-  echo "ERROR: Following return codes have been gathered:"
+  echo "ERROR: One or more items failed to either be retrieved from the smartplug or failed to send to the Zabbix server." >&2;
+  echo "ERROR: Following return codes have been gathered:" >&2;
+
+  # iterate over all gathered return codes
   for rc in "${itemFailedReturnCodes[@]}"; do
-    echo " - ${rc}"
+    echo " - ${rc}" >&2
   done
+
   exit 6;
 };
 
